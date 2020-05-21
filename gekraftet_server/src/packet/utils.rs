217eceb5b6faa7a3e6d65977dd1f3d0e412ea566
Hoe Hao Cheng <haochengho12907@gmail.com>
2020-05-21 22:36:50 +0800

    packet: refactor read_uc2 and read_utf8 out of receiver.rs

---
+++ gekraftet_server/src/packet/utils.rs
@@ -0,0 +1,35 @@
+use tokio::io::{AsyncReadExt, Error as IoError, ErrorKind, Result as IoResult};
+
+pub async fn read_utf8<I>(input: &mut I) -> IoResult<Box<str>>
+where
+    I: AsyncReadExt + Unpin,
+{
+    let len = input.read_i16().await?;
+    let mut bytes = Vec::with_capacity(len as usize);
+
+    input.take(len as u64).read_to_end(&mut bytes).await?;
+
+    let result = String::from_utf8(bytes)
+        .map_err(|e| IoError::new(ErrorKind::InvalidData, e))?
+        .into_boxed_str();
+
+    Ok(result)
+}
+
+pub async fn read_ucs2<I>(input: &mut I) -> IoResult<Box<str>>
+where
+    I: AsyncReadExt + Unpin,
+{
+    let len = input.read_i16().await?;
+    let mut code_units = Vec::with_capacity(len as usize);
+
+    for _ in 0..len {
+        code_units.push(input.read_u16().await?);
+    }
+
+    let result = String::from_utf16(code_units.as_ref())
+        .map_err(|e| IoError::new(ErrorKind::InvalidData, e))?
+        .into_boxed_str();
+
+    Ok(result)
+}
