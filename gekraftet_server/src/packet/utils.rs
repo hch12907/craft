ed10d197398d98c74921eea537570409b7222340
Hoe Hao Cheng <haochengho12907@gmail.com>
2020-05-27 17:21:47 +0800

    packet/utils: add utils for UCS2/UTF8 sending

--- gekraftet_server/src/packet/utils.rs
+++ gekraftet_server/src/packet/utils.rs
@@ -1,4 +1,4 @@
-use tokio::io::{AsyncReadExt, Error as IoError, ErrorKind, Result as IoResult};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, Error as IoError, ErrorKind, Result as IoResult};
 
 pub async fn read_utf8<I>(input: &mut I) -> IoResult<Box<str>>
 where
@@ -32,4 +32,28 @@ where
         .into_boxed_str();
 
     Ok(result)
+}
+
+pub async fn write_utf8<I, R>(input: &mut I, val: R) -> IoResult<()>
+where
+    I: AsyncWriteExt + Unpin,
+    R: AsRef<str>
+{
+    let bytes = val.as_ref().as_bytes();
+    input.write_i16(bytes.len() as i16).await?;
+    input.write_all(bytes).await?;
+    Ok(())
+}
+
+pub async fn write_ucs2<I, R>(input: &mut I, val: R) -> IoResult<()>
+where
+    I: AsyncWriteExt + Unpin,
+    R: AsRef<str>
+{
+    let code_point = val.as_ref().encode_utf16().collect::<Vec<_>>();
+    input.write_i16(code_point.len() as i16).await?;
+    for x in code_point.iter() {
+        input.write_u16(*x).await?;
+    }
+    Ok(())
 }
