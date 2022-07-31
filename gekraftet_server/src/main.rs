970001cdf5a844f3d34e2176abf6d722e5eaf6a1
Hoe Hao Cheng <haochengho12907@gmail.com>
2022-07-31 16:58:34 +0800

    server: update tokio

--- gekraftet_server/src/main.rs
+++ gekraftet_server/src/main.rs
@@ -1,5 +1,3 @@
-#![feature(bufreader_seek_relative)]
-
 mod config;
 mod packet;
 mod world;
@@ -7,13 +5,12 @@ mod world;
 use config::Config;
 use tokio::io::BufReader;
 use tokio::net::TcpListener;
-use tokio::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conf = Config::try_read()?;
     let mut listener = TcpListener::bind((conf.ip, conf.port)).await?;
-    
+
     loop {
         let (stream, addr) = listener.accept().await?;
         println!("received connection: {}", addr);
